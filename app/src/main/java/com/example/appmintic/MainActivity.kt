c3c727e3de5f979785af09dff4b1a78fd2850f44
Pablo <pablo.arodriguezgo@gmail.com>
2021-11-14 01:06:42 -0500

    cambios en errores

--- app/src/main/java/com/example/appmintic/MainActivity.kt
+++ app/src/main/java/com/example/appmintic/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ciudad = ArrayList()
-        ciudad?.add(ciudad(nombre = "Central Park", R.drawable.Times_Square_New_York_USA))
+        ciudad?.add(ciudad(nombre = "Central Park", R.drawable.times_square_new_york))
 
         lista=findViewById(R.id.ciudades)
 
